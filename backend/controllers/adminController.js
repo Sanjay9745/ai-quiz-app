207045713c4de6b09288095fa527edb42c5387ff
abhijith <abhijitht210@gmail.com>
2026-01-22 15:10:06 +0530

    Backend: admin login & registration

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,57 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// Admin Registration
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+      role: "admin",
+    });
+    await user.save();
+    res
+      .status(201)
+      .json({ success: true, message: "User registered successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+
+// Admin Login
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    console.log("REQ PASSWORD:", password);
+    console.log("DB PASSWORD:", user.password);
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" },
+    );
+    res.json({ success: true, token, message: "Login successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
